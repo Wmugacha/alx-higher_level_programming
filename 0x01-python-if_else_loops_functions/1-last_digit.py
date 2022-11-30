3144bf9c03866118c7e0d6cc9a9fa68f8ffdd27d
Wmugacha <wilfredmugacha@gmail.com>
2022-11-30 04:22:59 +0000

    fixed output error for 0./1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 
-l_digit = (number % 10):
-if (number < 0):
-    l_digit = (number % -10)
 if (number == 0):
     print(f"Last digit of {number} is {l_digit} and is 0")
-elif (l_digit > 5):
+elif (number < 0):
+    l_digit = (number % -10)
+else:
+    l_digit = (number % 10)
+
+if (l_digit > 5):
     print(f"Last digit of {number} is {l_digit} and is greater than 5")
 elif (l_digit == 0):
     print(f"Last digit of {number} is {l_digit} and is 0")
 elif (l_digit < 6 and l_digit != 0):
     print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
-else:
-    print
