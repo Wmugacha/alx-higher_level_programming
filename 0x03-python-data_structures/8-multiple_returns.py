22d46e23637c8c4319c7d12c463661542a7cb9c8
Wmugacha <wilfredmugacha@gmail.com>
2022-12-07 02:39:28 +0000

    Task 8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
     """function that returns a tuple with the
         length of a string and its first character"""
-
     if sentence != '':
         first_char = sentence[0]
     else:
