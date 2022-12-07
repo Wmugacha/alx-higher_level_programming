28e81f3a2e58477e9dbceae9e83f5a39d6c59140
Wmugacha <wilfredmugacha@gmail.com>
2022-12-07 02:07:47 +0000

    Task 6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """function that prints a matrix of integers"""
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print("{:d}".format(column), end="")
+            else:
+                print("{:d}".format(column), end=" ")
+        print()
+
