1291c2bcd5c74b1c670cb8847aefcc52c818711c
Wmugacha <wilfredmugacha@gmail.com>
2023-01-11 11:14:17 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Module that creates an empty class.
+"""
+
+
+class BaseGeometry:
+    """Functioon that creates an empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
