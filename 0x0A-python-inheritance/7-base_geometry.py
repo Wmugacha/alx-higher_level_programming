725ac2cb7192b148aa5ef6a2ee598a43030bf662
Wmugacha <wilfredmugacha@gmail.com>
2023-01-11 12:42:45 +0000

    task 7 errors fixed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,10 +9,19 @@ class BaseGeometry:
     """Functioon that creates an empty class"""
 
     def area(self):
+        """Method not yet implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+            validates value as an integer
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
