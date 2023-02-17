304f6632583fa94b2d95f37540bbe353f935a7f3
Wmugacha <wilfredmugacha@gmail.com>
2023-02-17 11:30:25 +0000

    task 19

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,8 @@
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.
+
+SELECT DISTINCT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id AND tv_shows.title = 'Dexter'
+WHERE tv_shows.id IS NULL
+ORDER BY name ASC;
---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT tv_shows.title, SUM(tv_show_rating.rating)
+FROM tv_show
+INNER JOIN tv_show_rating
+ON tv_show.id = tv_show_rating.show_id
+ORDER BY rating DESC;
