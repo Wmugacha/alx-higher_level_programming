c2e116bc92e64fdfb24019c59f98a3653fac47ea
Wmugacha <wilfredmugacha@gmail.com>
2023-02-17 11:37:09 +0000

    task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title, SUM(tv_show_rating.rating)
 FROM tv_show
 INNER JOIN tv_show_rating
 ON tv_show.id = tv_show_rating.show_id
-GROUP BY tv_shows.title
+GROUP BY title
 ORDER BY rating DESC;
