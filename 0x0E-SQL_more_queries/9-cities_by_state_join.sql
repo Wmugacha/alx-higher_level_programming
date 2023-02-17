2e4fa9392501dbf6a78da8238f409eb9ac685d16
Wmugacha <wilfredmugacha@gmail.com>
2023-02-17 06:17:31 +0000

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
-SELECT *
-FROM states
-NATURAL JOIN cities
-ORDER BY `id`;
+SELECT cities.id, cities.name, states.name
+FROM states INNER JOIN cities
+    ON states.id = cities.state_id
+    ORDER BY `id`;
+
