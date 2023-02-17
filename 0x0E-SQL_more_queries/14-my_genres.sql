3b5aab5cc92d37ac8a02eed0746c72405a223f69
Wmugacha <wilfredmugacha@gmail.com>
2023-02-17 09:25:03 +0000

    task 14 - Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,4 +10,4 @@ FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.id = 8
-ORDER BY name;
+ORDER BY name ASC;
