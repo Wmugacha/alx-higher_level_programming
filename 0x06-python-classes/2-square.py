4cde0fc20fca176baa68b25363363ae261121d7e
Wmugacha <wilfredmugacha@gmail.com>
2023-03-10 09:51:22 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+"""Module that defines a square"""
 
 
 class Square:
     """Class that defines a Square"""
     def __init__(self, size=0):
-        self.__size = size
+        """
+            Initializing this square class.
+        """
 
         if type(size) is not int:
             raise TypeError ("size must be an integer")
 
         if size < 0:
             raise ValueError ("size must be >= 0")
+
+        self.__size = size
