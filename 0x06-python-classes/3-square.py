f51e228fcff1f6720762baafe4ccbc66ab1824cf
Wmugacha <wilfredmugacha@gmail.com>
2023-03-10 12:20:43 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines a Square"""
+
+
+class Square:
+    """Class that defines a Square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class.
+
+        Args:
+            size: represents the size of the square defined.
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than zero.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """method that returns area of the square.
+
+        Returns: Area of the square
+        """
+
+        return (self.__size ** 2)
