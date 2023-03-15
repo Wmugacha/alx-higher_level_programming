d41a1d0c673813d60697db4b4778866b37eb1ed5
Wmugacha <wilfredmugacha@gmail.com>
2023-03-15 04:53:43 +0000

    task 10 - factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that computes and prints a factorial
+
+function factorial (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = Number.parseInt(process.argv[2]);
+
+console.log(factorial(arg));
