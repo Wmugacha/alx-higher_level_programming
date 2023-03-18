bc0944b4447073798602df0a95d97bfe3c529044
Wmugacha <wilfredmugacha@gmail.com>
2023-03-18 09:42:25 +0000

    task 2 - empty oobject

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Module that defines a Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+};
