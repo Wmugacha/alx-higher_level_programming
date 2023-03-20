8d90f8a8e80b16315a8976d45e45ef23b8d4b48d
Wmugacha <wilfredmugacha@gmail.com>
2023-03-20 10:17:58 +0000

    task 2 - select states that start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 script that lists all states from
-the database hbtn_0e_0_usa.
+the database hbtn_0e_0_usa with
+a name starting with N.
 
 """
 from sys import argv
@@ -15,7 +16,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY
+                'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
