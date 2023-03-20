4f9bd8acac0702b97c760403d30ab3e4cfc58542
Wmugacha <wilfredmugacha@gmail.com>
2023-03-20 11:24:00 +0000

    task 2 - search for a state with an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Access the database and get the states.
+    """
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            port=3306,
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except MySQLdb.Error:
+        print("error connecting")
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY\
+                    states.id", (argv[4],))
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except MySQL.Error:
+        print("Execuction failed")
+
+        cur.close()
+        db.close()
