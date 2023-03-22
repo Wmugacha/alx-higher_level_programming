b86ebd5f089c0b51e3ff3dcfce451d9df758d42b
Wmugacha <wilfredmugacha@gmail.com>
2023-03-22 10:50:41 +0000

    Apply all MySQLdb lessons

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script that lists states from
+the database hbtn_0e_4_usa.
+
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Access the database and get the states.
+    """
+    state_name = argv[4]
+
+    query = "SELECT cities.name \
+             FROM cities INNER JOIN states \
+             ON cities.state_id = states.id \
+             WHERE states.name LIKE BINARY %s \
+             ORDER BY cities.id ASC"
+
+    args = (state_name,)
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+            )
+
+    cur = db.cursor()
+    cur.execute(query, args)
+    row = cur.fetchall()
+    print(row)
+
+    cur.close()
+    db.close()
