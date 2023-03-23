e6c221f32382877c87ac93cab6b0a22a6bef035d
Wmugacha <wilfredmugacha@gmail.com>
2023-03-23 18:22:50 +0000

    task 8 - print first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all State objects from
+script that lists State objects from
 the database hbtn_0e_6_usa.
 """
 import sys
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     """
     Get the states from the database
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     if count == 0:
         print('Nothing')
     else:
-        for instance in session.query(State).filter(State.id == 1)
-        .order_by(State.id):
+        for instance in session.query(State).filter(State.id == 1) \
+                .order_by(State.id):
             print("{0}: {1}".format(instance.id, instance.name))
