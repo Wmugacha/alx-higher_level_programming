5dd9688cec6d71fa3487270aadc01b5d53f11440
Wmugacha <wilfredmugacha@gmail.com>
2023-03-31 19:58:35 +0000

    task 0 - fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,8 @@ Python script that fetches https://alx-intranet.hbtn.io/status.
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
             as response:
-        html = response.read()
+        resp = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(resp)))
+        print("\t- content: {}".format(resp))
+        print("\t- utf8 content: {}".format(resp.decode("utf-8")))
