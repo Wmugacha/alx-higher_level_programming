b9b621e493a784420907c69e23541ff904d20a6f
Wmugacha <wilfredmugacha@gmail.com>
2023-04-04 17:34:35 +0000

    post request with json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL.
-curl -s -X POST -H "content-type: application/json" -d "$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat $2)" "$1"
