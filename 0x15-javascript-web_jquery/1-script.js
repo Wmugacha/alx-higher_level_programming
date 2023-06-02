db0db20f958ad8ed95f5bd30e8e0aa3ba7c23f34
Wmugacha <wilfredmugacha@gmail.com>
2023-06-02 09:28:29 +0000

    change text color

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+// Update text color of header element to red
+
+const headers = document.getElementsByTagName('header')
+
+for (const i = 0; i < headers.length; i++) {
+    headers[i]
+.style.color = '#FF0000';
+}
